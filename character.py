9b21c4f720118f6fb3ef4638f0020b3596ed37c9
Jake Shasteen <jake@jetownersanonymous.com>
2015-07-18 13:44:29 -0400

    We have a working game! Got rid of obvious bugs.

---
+++ character.py
@@ -0,0 +1,55 @@
+import random
+from combat import Combat
+
+class Character( Combat ):
+    experience = 0
+    base_hit_points = 10
+    attack_limit = 10
+
+    def attack( self ):
+        roll = random.randint( 1, self.attack_limit )
+        if self.weapon == 'sword':
+            roll += 1
+        elif self.weapon == 'axe':
+            roll += 2
+        return roll > 4
+
+    def dodge( self ):
+        roll = random.randint( 1, self.dodge_limit )
+        if self.weapon == 'sword':
+            roll += 1
+        if self.weapon == 'bow':
+            roll += 2
+        return roll > 4
+
+    def get_weapon( self ):
+        weapon_choice = input("Weapon [s]word, [a]xe, or [b]ow:").lower()
+        
+        if weapon_choice in 'sab':
+            if weapon_choice == 's':
+                return 'sword'
+            elif weapon_choice == 'a':
+                return 'axe'
+            elif weapon_choice =='b':
+                return 'bow'
+        else:
+            return self.get_weapon()
+
+    def __init__( self, **kwargs ):
+        self.name = input("Name: ")
+        self.weapon = self.get_weapon()
+        self.hit_points = self.base_hit_points
+
+        for key, value in kwargs.items():
+            setattr( self, key, value )
+
+    def __str__( self ):
+        return '{}, HP {}, XP: {}'.format( self.name, self.hit_points, self.experience )
+
+    def rest( self ):
+        if self.hit_points < self.base_hit_points:
+            self.hit_points +=1
+
+    def leveled_up( self ):
+        return self.experience >= 5
+
---
+++ game.py
@@ -0,0 +1,73 @@
+from character import Character
+from monster import Dragon, Goblin, Troll
+
+class Game:
+    def setup( self ):
+        self.player = Character()
+        self.monsters = [
+                Goblin(),
+                Troll(),
+                Dragon()
+                ]
+        self.monster = self.get_next_monster()
+
+    def get_next_monster( self ):
+        try:
+            return self.monsters.pop(0)
+        except IndexError: #no more monsters!
+            return None
+
+    def monster_turn( self ):
+        if self.monster.attack():
+            if self.player.dodge():
+                print("The {} swipes at you, but you dodge out of the way.".format( self.monster.__class__.__name__ ))
+            else:
+                print("The {} swipes at you, drawing blood!".format( self.monster.__class__.__name__ ))
+                self.player.hit_points -= 1
+        else:
+            print("The {} tries to hit you, but misses.".format( self.monster.__class__.__name__ ))
+
+
+    def player_turn( self ):
+        choice = input("[A]ttack, [R]est, or [Q]uit? ").lower()
+
+        if choice in 'arq':
+            if choice == 'a': #attack
+                if self.player.attack(): 
+                    if self.monster.dodge(): #monster dodged
+                        print("The {} dodged out of the way of your {}!".format( self.monster.__class__.__name__, self.player.weapon ))
+                    else: #hit
+                        print("You hit the {} with your {}!".format( self.monster.__class__.__name__, self.player.weapon ))
+                        self.monster.hit_points -= 1
+                else: #miss
+                    print("You miss with your {}.".format( self.player.weapon ))
+            elif choice == 'r': #rest
+                self.player.rest()
+            elif choice == 'q': #quit
+                quit() #exit program
+        else: #player did not choose an approved choice
+            return self.player_turn() #loop back to choice
+
+    def cleanup( self ):
+        if self.monster.hit_points <= 0:
+            self.player.experience += 1
+            print("You slew the {} and gained an experience!".format( self.monster.__class__.__name__ ))
+            self.monster = self.get_next_monster()
+
+
+    def __init__( self ):
+        self.setup()
+
+        while self.player.hit_points and ( self.monster or self.monsters ):
+            print( self.player )
+            print( self.monster )
+            self.monster_turn()
+            self.player_turn()
+            self.cleanup()
+
+        if self.player.hit_points:
+                print("You win!")
+        elif self.monsters or self.monster:
+                print("You lose!")
+
+
